61b48d4abf0b0bf4bae24c36d021dbb862b378fd
Doc <alessio.caldarigi@gmail.com>
2021-08-11 14:39:25 +0200

    Jk this one si the last

--- src/main/java/com/example/securitydemo/SecurityConfig.java
+++ src/main/java/com/example/securitydemo/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/login").permitAll()// I am allowing to everyone, to call every service under /login, /login included
-                .antMatchers("/protected").authenticated()// I am allowing only authenticated users to call the all the service under /
+                .antMatchers("/**").authenticated()// I am allowing only authenticated users to call the all the service under /
                 .and().formLogin();//.failureHandler(customAuthenticationFailureHandler());
     }
 }
